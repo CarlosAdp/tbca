8fdf20d074f3efd19e94170ef513f266f6bca305
Carlos Alberto Duarte Pinto <carlos.adpinto@gmail.com>
2023-06-18 14:47:43 -0300

    Build initial package

---
+++ setup.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+from setuptools import setup
+
+from tbca import __prog__, __version__, __author__, __email__, __url__, \
+    __description__, __keywords__, __license__
+
+
+setup(
+    name=__prog__,
+    version=__version__,
+    author=__author__,
+    author_email=__email__,
+    description=__description__,
+    long_description=open(Path(__file__).parent / 'README.md').read(),
+    long_description_content_type='text/markdown',
+    license=__license__,
+    url=__url__,
+    keywords=__keywords__,
+    classifiers=[
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Intended Audience :: Other Audience'
+    ],
+    packages=['tbca'],
+    package_dir={'tbca': 'tbca'},
+    install_requires=[
+    ],
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+        'dev': [
+            'flake8',
+            'mypy'
+        ]
+    }
+)
---
+++ tbca/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+__prog__ = 'tbca'
+__author__ = 'Carlos Pinto'
+__email__ = 'carlos.adpinto@gmail.com'
+__url__ = 'https://github.com/CarlosAdp/tbca'
+__license__ = 'Apache License 2.0'
+__description__ = 'Scraper e API de dados da Tabela Brasileira de Composição de Alimentos'
+__keywords__ = 'api scraper brasil alimentos nutricao gastronomia dieta'
+__version__ = '0.1.0'
